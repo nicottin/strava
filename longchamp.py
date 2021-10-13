4f3ef0132af0b1bde96f55d4023b5e3a0efa2749
I054301 <nicolas.cottin@sap.com>
2021-10-13 14:22:21 +0200

    Add longchamp regular segment

--- longchamp.py
+++ longchamp.py
@@ -20,49 +20,61 @@ if __name__ == "__main__":
                     email=cred_dict["user"],
                     password=cred_dict["password"])
 
-    lonlon_day = {}
-    record = None
-    for year in list(range(int(config_dict["year_start"]), datetime.datetime.now().year + 1)):
-        lonlon_effort = client.get_segment_efforts(3572501, start_date_local=f"{year}-01-01", end_date_local=f"{year}-12-31")
-        for l in lonlon_effort:
-            if record == None or l.moving_time < record.moving_time :
-                record = l
-            if not f"{l.start_date}"[:10] in lonlon_day:
-                lonlon_day[f"{l.start_date_local}"[:10]] = {"count": 0, "distance": 0.0, "time": 0}
-            lonlon_day[f"{l.start_date}"[:10]]["count"] += 1
-            lonlon_day[f"{l.start_date}"[:10]]["distance"] += float(l.distance)
-            lonlon_day[f"{l.start_date}"[:10]]["time"] += l.moving_time.total_seconds()
-
-    nb_rides = 0
-    lonlon_list = []
-    for day in lonlon_day:
-        nb_rides += 1
-        lonlon_day[day]["average_speed"] =  round((lonlon_day[day]['distance'] / 1000) / (lonlon_day[day]['time'] / 3600),2)
-        lonlon_list.append({"day": day, **lonlon_day[day]})
-
-    print(f"\nTotal nombre de sorties à Longchamp: {nb_rides}")
-
-    record_avg = round((float(record.distance) / 1000) / (record.moving_time.total_seconds() / 3600),2)
-    print(f"\nRecord: {record.moving_time} ({record_avg} km/h), le {record.start_date_local}")
-
-    lonlon_list_speed = sorted(lonlon_list, key=lambda x: x["average_speed"], reverse=True)
-    rank = 0
-    for l in lonlon_list_speed:
-        rank += 1
-        l["rank"] = rank
-
-    print(f"\nTop {config_dict['top']}")
-    print("---------------------------------------\n")
-
-    for i, d in enumerate(lonlon_list_speed):
-        print(f"{i+1}) {d['day']}: {d['count']} tours | {d['average_speed']} km/h")
-        if i >= int(config_dict["top"]) - 1:
-            break
+    segments = [3572501, 17109958]
+    # segments = [17109958]
     
-    print(f"\n{config_dict['top']} Dernières")
-    print("----------------------------------------------------------\n")
-    lonlon_list_date = sorted(lonlon_list_speed, key=lambda x: x["day"], reverse=True)
-    for i, d in enumerate(lonlon_list_date):
-        print(f"{i+1}) {d['day']}: {d['count']} tours | {d['average_speed']} km/h | Classement {d['rank']}/{nb_rides}")
-        if i >= int(config_dict["top"]) - 1:
-            break
+    for seg in segments:
+        lonlon_day = {}
+        record = None
+        for year in list(range(int(config_dict["year_start"]), datetime.datetime.now().year + 1)):
+            lonlon_efforts = []
+            date_ranges = [
+                {"start": "01-01", "end": "03-31"},
+                {"start": "04-01", "end": "06-30"},
+                {"start": "07-01", "end": "09-30"},
+                {"start": "10-01", "end": "12-31"}
+            ]
+            for date_range in date_ranges:
+                lonlon_effort = client.get_segment_efforts(seg, start_date_local=f"{year}-{date_range['start']}", end_date_local=f"{year}-{date_range['end']}")
+                for l in lonlon_effort:
+                    if record == None or l.moving_time < record.moving_time :
+                        record = l
+                    if not f"{l.start_date}"[:10] in lonlon_day:
+                        lonlon_day[f"{l.start_date_local}"[:10]] = {"count": 0, "distance": 0.0, "time": 0}
+                    lonlon_day[f"{l.start_date}"[:10]]["count"] += 1
+                    lonlon_day[f"{l.start_date}"[:10]]["distance"] += float(l.distance)
+                    lonlon_day[f"{l.start_date}"[:10]]["time"] += l.moving_time.total_seconds()
+
+        nb_rides = 0
+        lonlon_list = []
+        for day in lonlon_day:
+            nb_rides += 1
+            lonlon_day[day]["average_speed"] =  round((lonlon_day[day]['distance'] / 1000) / (lonlon_day[day]['time'] / 3600),2)
+            lonlon_list.append({"day": day, **lonlon_day[day]})
+
+        print(f"\nTotal nombre de sorties à Longchamp: {nb_rides}")
+
+        record_avg = round((float(record.distance) / 1000) / (record.moving_time.total_seconds() / 3600),2)
+        print(f"\nRecord: {record.moving_time} ({record_avg} km/h), le {record.start_date_local}")
+
+        lonlon_list_speed = sorted(lonlon_list, key=lambda x: x["average_speed"], reverse=True)
+        rank = 0
+        for l in lonlon_list_speed:
+            rank += 1
+            l["rank"] = rank
+
+        print(f"\nTop {config_dict['top']}")
+        print("---------------------------------------\n")
+
+        for i, d in enumerate(lonlon_list_speed):
+            print(f"{i+1}) {d['day']}: {d['count']} tours | {d['average_speed']} km/h")
+            if i >= int(config_dict["top"]) - 1:
+                break
+        
+        print(f"\n{config_dict['top']} Dernières")
+        print("----------------------------------------------------------\n")
+        lonlon_list_date = sorted(lonlon_list_speed, key=lambda x: x["day"], reverse=True)
+        for i, d in enumerate(lonlon_list_date):
+            print(f"{i+1}) {d['day']}: {d['count']} tours | {d['average_speed']} km/h | Classement {d['rank']}/{nb_rides}")
+            if i >= int(config_dict["top"]) - 1:
+                break
